4aa314d237839e6f7c7c6aa0f8f3bd727b2f7fb2
BitSails <BitSails@users.noreply.github.com>
2017-01-27 19:49:12 -0400

    Update search.cpp
    Switched from using end-of-file to terminate input to the use of a sentinel. Cleaned up a few comments and print statements.

--- search.cpp
+++ search.cpp
@@ -12,8 +12,7 @@ using namespace std;
 \param [in] data The data set that will be searched
 \returns location of key if found or -1 if not found
 */
-
-//auto linearSearch...
+int linearSearch(auto data, auto key);//prototype
 
 
 int main()
@@ -23,40 +22,48 @@ int main()
   int result;
 
    cout<<"Welcome to \"search it\". We first need some input data."<<endl;
-   cout<<"We'll assume the inputs do not have any spaces."<<endl;
-   cout<<"\nTo end input type Ctrl + D"<<endl<<endl;
+   cout<<"We'll assume the inputs do not have any spaces."<<endl<<endl;
+   cout<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
 
-    while(cin>>input)//read an unknown number of inputs
+   cin>>input;
+ 
+    while(input != "#")//read an unknown number of inputs from keyboard
     {
        inputs.push_back(input);
+       cin>>input;
     }
 
    cout<<endl<<"["<<inputs.size()<<" values read from input source]"<<endl<<endl;
 
   if(inputs.size() == 0)//no input
-   exit(1);//nothing to do but quit program
-
- cout<<"\nTo quit the program type Ctrl + D"<<endl<<endl;
+  {
+      cout<<endl<<"No input received, quiting..."<<endl<<endl;
+       exit(1);//nothing to do but quit program
+  }
+ 
+   cout<<endl<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
   cout<<"Enter a value to search for: ";
 
-     cin.clear();//not needed when switching to reading from file
 
-    while(cin>>search_key)//perform searches until Ctrl + D entered
+   cin>>search_key;
+ 
+    while(search_key != "#")//perform searches until sentinel entered
     {
         result = linearSearch(inputs,search_key);
 
-        cout<<"  "<<search_key<<" was ";
+        cout<<"  '"<<search_key<<"' was ";
 
         if (result == -1)
           cout<<"not found";
         else
-          cout<<"found at position "<<result;
+          cout<<"found at index "<<result;
 
 
         cout<<endl<<endl<<"Enter a value to search for: ";
-
+        cin>>search_key; 
     }
 
+   cout<<endl<<"Program \"search it\" is now finished."<<endl<<endl;
 
     return 0;
 }
